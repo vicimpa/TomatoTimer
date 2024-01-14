e807d0e3ee76d9ce38dcf652ba7edcc9afaf98a4
PromiSe#### <vicimpa@ya.ru>
2024-01-15 00:16:31 +0700

    Append CI/CD

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,20 @@
 import { defineConfig } from "vite";
 import paths from "vite-tsconfig-paths";
+import { viteSingleFile } from "vite-plugin-singlefile";
 
 export default defineConfig({
   root: './src',
+  base: './',
   publicDir: '../public',
   build: {
-    outDir: '../dist'
+    outDir: '../dist',
+    emptyOutDir: true
   },
   server: {
     host: '0.0.0.0'
   },
   plugins: [
-    paths({ root: '../' })
+    paths({ root: '../' }),
+    viteSingleFile()
   ]
 });
